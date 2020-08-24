8cc2e467186cd96ec2aee08faacb4c2029c712a4
share-teamproject <projshaina@gmail.com>
2020-08-24 09:16:58 +0900

    회원가입 모바일

--- new_js/ui_m.js
+++ new_js/ui_m.js
@@ -256,6 +256,26 @@ $(function(){
 	$('.zoneBox').hide();
 	$('.zoneBox').eq(idx).show();
 	});
+	
+	$(function(){
+		// 팝업 호출
+		$('.popactive').click(function(){
+			var data = $(this).data('popactive');
+			$('body').css('position','fixed');
+			$('body').css('height','100%');
+			$('.layerfullWrap').css('display','none');
+			$('[data-fullpop="'+ data +'"]').css('display','block');
+		});
+	
+		// 팝업 닫기
+		$('.layerCloseBtn').click(function(){
+			$('body').removeAttr('style');
+			$(this).closest('.layerfullWrap').css('display','none');
+		});
+	});
+
+
+
 });
 
 /* ==============================
